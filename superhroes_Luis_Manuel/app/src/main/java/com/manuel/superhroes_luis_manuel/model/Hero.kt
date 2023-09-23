05d5ca2f7ee17b6d725ccba48dd08efe0420a594
Manuel <s20120179@alumnos.itsur.edu.mx>
2023-09-23 17:57:23 -0600

    Creación de model para la utilización de variables

---
+++ superhroes_Luis_Manuel/app/src/main/java/com/manuel/superhroes_luis_manuel/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.manuel.superhroes_luis_manuel.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
