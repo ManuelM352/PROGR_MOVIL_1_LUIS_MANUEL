9d4a8121dd7afc3fa1e4e1987b43f649f3a66bc0
Manuel <s20120179@alumnos.itsur.edu.mx>
2023-09-23 17:16:23 -0600

    Configuración del proyecto y creación de los datos ficticios para los superhéroes

---
+++ superhroes_Luis_Manuel/app/src/main/java/com/manuel/superhroes_luis_manuel/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.manuel.superhroes_luis_manuel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.manuel.superhroes_luis_manuel.ui.theme.Superhéroes_Luis_ManuelTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Superhéroes_Luis_ManuelTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Superhéroes_Luis_ManuelTheme {
+        Greeting("Android")
+    }
+}
